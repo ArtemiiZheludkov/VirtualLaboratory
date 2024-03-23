17c4c56c8329a0eb8f136b49ea8acc2ff647ea45
Artemii Zheludkov <artmjeludkov@gmail.com>
2024-03-24 00:51:19 +0200

    add data

---
+++ Assets/Scripts/Lab2/Data/DataContainer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace VirtualLaboratory.Lab2
+{
+    public class DataContainer : MonoBehaviour
+    {
+        private DataProvider _dataProvider;
+        
+        private List<float> Uz;
+        private Dictionary<int, List<float>> Iz;
+
+        private int _variant;
+
+        private void Start()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            _dataProvider = new DataProvider();
+            _variant = 1;
+            
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Uz = _dataProvider.GetData(_variant, "Uz(V)");
+            Iz = new Dictionary<int, List<float>>();
+            
+            float[] Ip_values = { 0.1f, 0.15f, 0.2f, 0.25f, 0.3f, 0.35f, 0.4f };
+            foreach (float Ip in Ip_values)
+            {
+                string requestString = $"Ip={Ip.ToString(CultureInfo.InvariantCulture)}(mA)";
+                Iz.Add((int)(Ip*100), _dataProvider.GetData(_variant, requestString));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Lab2/Data/DataProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace VirtualLaboratory.Lab2
+{
+    public class DataProvider
+    {
+        public List<float> GetData(int variant, string name)
+        {
+            try
+            {
+                Debug.Log($"Try load: {name}");
+                
+                string resourcePath = $"lab2/lab2_variant_{variant}";
+                
+                TextAsset dataFile = Resources.Load<TextAsset>(resourcePath);
+                
+                string[] lines = dataFile.text.Split('\n');
+                List<float> data = new List<float>();
+            
+                foreach (string line in lines)
+                {
+                    string trimmedLine = line.Trim();
+                    if (string.IsNullOrEmpty(trimmedLine))
+                        continue;
+
+                    string[] values = trimmedLine.Split(',');
+                    if (values[0].Trim().Equals(name, StringComparison.OrdinalIgnoreCase))
+                    {
+                        for (int i = 1; i < values.Length; i++)
+                        {
+                            if (float.TryParse(values[i], NumberStyles.Any, CultureInfo.InvariantCulture, out float parsedValue))
+                                data.Add(parsedValue);
+                        }
+                        
+                        Debug.Log($"Data loaded: {name}\n" + $"Count = {data.Count}");
+                        break;
+                    }
+                }
+
+                return data;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Error load data: {ex}");
+                return null;
+            }
+        }
+    }
+}
