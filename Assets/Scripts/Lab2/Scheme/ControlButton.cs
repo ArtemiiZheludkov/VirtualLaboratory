704116c645500caaf1b84d61deb060d336d7dd01
Artemii Zheludkov <artemii.zheludkov@gmail.com>
2024-03-26 19:19:27 +0200

    add control experiment

---
+++ Assets/Scripts/Lab2/Scheme/ControlButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace VirtualLaboratory.Lab2
+{
+    public class ControllButton : MonoBehaviour
+    {
+        [Header("MENU")]
+        [SerializeField] private GameObject _blockPanel;
+        [SerializeField] private Button _start;
+        [SerializeField] private Button _stop;
+    }
+}
